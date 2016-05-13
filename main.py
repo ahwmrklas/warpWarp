3553997188ba7a976d17b5648b580bf61f83a3b4
rex beard <ahw@mrklas.com>
2016-05-12 22:39:56 -0400

    Added menus

--- main.py
+++ main.py
@@ -16,9 +16,55 @@ from overlay import *
 # PURPOSE: Button handler. The Quit button
 #          call this when "Quit" button clicked
 # RETURNS: I don't know.
-def correct_quit(tk):
+def exitProgram(tk):
+    print("quitMain")
     tk.destroy()
     tk.quit()
+
+# I don't like these. They don't seem very objecty
+# Perhaps each of them should be a class?
+def newGame():
+    print("newGame")
+
+# I don't like these. They don't seem very objecty
+def openGame():
+    print("openGame")
+
+# I don't like these. They don't seem very objecty
+def saveGame():
+    print("saveGame")
+
+# I don't like these. They don't seem very objecty
+def aboutHelp():
+    print("aboutHelp")
+
+# I don't like these. They don't seem very objecty
+def helpHelp():
+    print("helpHelp")
+
+# PURPOSE: Create menu GUI elements
+# RETURNS: none
+def addMenus(tk):
+    menuBar = Menu(tk)
+
+    fileMenu = Menu(menuBar)
+
+    fileMenu.add_command(label="New", command=newGame)
+    fileMenu.add_command(label="Open", command=openGame)
+    fileMenu.add_command(label="Save", command=saveGame)
+    fileMenu.add_separator()
+    fileMenu.add_command(label="Exit", command=lambda:exitProgram(tk))
+
+    menuBar.add_cascade(label="File", menu=fileMenu)
+
+    helpMenu = Menu(menuBar)
+    helpMenu.add_command(label="About", command=aboutHelp)
+    helpMenu.add_command(label="Help", command=helpHelp)
+
+    menuBar.add_cascade(label="Help", menu=helpMenu)
+
+    tk.config(menu=menuBar)
+
 
 # PURPOSE: Just make a function out of the main code. It doesn't
 #          seem right without that.
@@ -28,6 +74,9 @@ def main():
     # Instance of tkinter to do GUI stuff
     tk = Tk()
 
+    # menu bar
+    addMenus(tk)
+
     # create a hex map that is the basis of our game display
     hexMap = HexagonalGrid(tk, scale = 20, grid_width=30, grid_height=20)
 
@@ -35,24 +84,13 @@ def main():
     hexMap.grid(row=0, column=0, padx=10, pady=10)
 
     # Create a quit button (obviously to exit the program)
-    quit = Button(tk, text = "Quit", command = lambda :correct_quit(tk))
+    quit = Button(tk, text = "Quit", command = lambda :exitProgram(tk))
 
     # Locate the button on the tkinter "grid"
     quit.grid(row=1, column=0)
 
     # display the whole hexMap.
     hexMap.drawGrid('blue')
-
-    #hexMap.setCell(0,0, fill='blue')
-    #hexMap.setCell(1,0, fill='red')
-    #hexMap.setCell(0,1, fill='green')
-    #hexMap.setCell(1,1, fill='yellow')
-    #hexMap.setCell(2,0, fill='cyan')
-    #hexMap.setCell(0,2, fill='teal')
-    #hexMap.setCell(2,1, fill='silver')
-    #hexMap.setCell(1,2, fill='white')
-    #hexMap.setCell(2,2, fill='gray')
-    #hexMap.setCell(3,3, fill='fuchsia')
 
     hexMap.setCell(4,4, fill='pink', content=TileContent.BASIC_SHIP)
 
